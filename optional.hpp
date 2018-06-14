f9c06a84c701a41d166b91af713aa6a77ec604ce
ho-ri1991 <hage.1991@hotmail.co.jp>
2018-06-15 00:23:30 +0900

    add comparator for optional

--- optional.hpp
+++ optional.hpp
@@ -10,14 +10,14 @@
 namespace my
 {
   template <class T>
-  struct DynamicStoragePolicy
+  struct DynamicStorage
   {
   private:
     T* data;
   public:
-    DynamicStoragePolicy(): data(NULL) {}
+    DynamicStorage(): data(NULL) {}
     template <class U>
-    DynamicStoragePolicy(const U& val): data(new T(val)) {}
+    DynamicStorage(const U& val): data(new T(val)) {}
   public:
     template <class U>
     void construct(const U& val)
@@ -37,15 +37,15 @@ namespace my
   };
 
   template <class T>
-  struct LocalStoragePolicy
+  struct LocalStorage
   {
   private:
     type_traits::aligned_storage<sizeof(T), type_traits::alignment_of<T>::value> storage;
     T* data; // for avoiding UB, std::launder is needed to remove this redundant pointer
   public:
-    LocalStoragePolicy(): data(NULL) {}
+    LocalStorage(): data(NULL) {}
     template <class U>
-    LocalStoragePolicy(const U& val): data(new(storage.address()) T(val)) {}
+    LocalStorage(const U& val): data(new(storage.address()) T(val)) {}
   public:
     template <class U>
     void construct(const U& val)
@@ -72,62 +72,63 @@ namespace my
 
   extern nullopt_t nullopt;
 
-  template <class T, template <class> class StoragePolicy = LocalStoragePolicy>
-  class optional: private StoragePolicy<T>
+  template <class T, template <class> class Storage = LocalStorage>
+  class optional
   {
   private:
+    Storage<T> storage;
     // for conversion to unspecified bool idiom
     typedef void (optional::*bool_type)(optional&);
   public:
     typedef T value_type;
   public:
+    bool has_value() const MY_NOEXCEPT { return storage.is_constructed(); }
+    operator bool_type() const MY_NOEXCEPT { return this->has_value() ? &optional::swap : 0; }
     void reset() MY_NOEXCEPT
     {
-      if (this->is_constructed()) this->destruct();
+      if (this->has_value()) storage.destruct();
     }
     T& operator*() MY_NOEXCEPT
     {
-      assert(this->is_constructed() && "null optional dereferenced");
-      return *this->address();
+      assert(this->has_value() && "null optional dereferenced");
+      return *storage.address();
     }
     const T& operator*() const MY_NOEXCEPT
     {
-      assert(this->is_constructed() && "null optional dereferenced");
-      return *this->address();
+      assert(this->has_value() && "null optional dereferenced");
+      return *storage.address();
     }
-    T* operator->() MY_NOEXCEPT { return this->address(); }
-    const T* operator->() const MY_NOEXCEPT { return this->address(); }
-    bool has_value() const MY_NOEXCEPT { return this->is_constructed(); }
+    T* operator->() MY_NOEXCEPT { return storage.address(); }
+    const T* operator->() const MY_NOEXCEPT { return storage.address(); }
     T& value()
     {
-      if (this->is_constructed()) return *this->address();
+      if (this->has_value()) return *(*this);
       else throw std::runtime_error("null optional dereferenced");
     }
     const T& value() const
     {
-      if (this->is_constructed()) return *this->address();
+      if (this->has_value()) return *(*this);
       else throw std::runtime_error("null optional dereferenced");
     }
     template <class U>
-    T value_or(const U& val) const { return this->has_value() ? *(this->address()) : val; }
-    operator bool_type() const MY_NOEXCEPT { return this->has_value() ? &optional::swap : 0; }
+    T value_or(const U& val) const { return this->has_value() ? *(*this) : val; }
     // TODO make this swap exception safe
     void swap(optional& other)
     {
       using std::swap;
-      if (this->is_constructed() && other.is_constructed())
+      if (this->has_value() && other.has_value())
       {
         swap(*(*this), *other);
       }
-      else if (!this->is_constructed() && other.is_constructed())
+      else if (!this->has_value() && other.has_value())
       {
-        this->construct(*other);
-        other.destruct();
+        storage.construct(*other);
+        other.reset();
       }
-      else if (this->is_constructed() && !other.is_constructed())
+      else if (this->has_value() && !other.has_value())
       {
-        other.construct(*(*this));
-        this->destruct();
+        other.storage.construct(*(*this));
+        this->reset();
       }
     }
 
@@ -135,15 +136,15 @@ namespace my
     optional() MY_NOEXCEPT {}
     optional(nullopt_t) MY_NOEXCEPT {}
     template <class U>
-    optional(const U& val): StoragePolicy<T>(val) {}
+    optional(const U& val): storage(val) {}
     optional(const optional& other)
     {
-      if (other) this->construct(*other);
+      if (other) storage.construct(*other);
     }
     template <class U, template <class> class OtherStoragePolicy>
     optional(const optional<U, OtherStoragePolicy>& other)
     {
-      if (other) this->construct(*other);
+      if (other) storage.construct(*other);
     }
     optional& operator=(nullopt_t) MY_NOEXCEPT
     {
@@ -153,8 +154,8 @@ namespace my
     template <class U>
     optional& operator=(const U& val)
     {
-      if (this->has_value()) *(this->address()) = val;
-      else this->construct(val);
+      if (this->has_value()) *(*this) = val;
+      else storage.construct(val);
       return *this;
     }
     optional& operator=(const optional& other)
@@ -165,11 +166,11 @@ namespace my
       }
       else if (!this->has_value() && other.has_value())
       {
-        this->construct(*other);
+        storage.construct(*other);
       }
       else if (this->has_value() && !other.has_value())
       {
-        this->destruct();
+        this->reset();
       }
       return *this;
     }
@@ -182,22 +183,113 @@ namespace my
 			}
 			else if (!this->has_value() && other.has_value())
 			{
-				this->construct(*other);
+				storage.construct(*other);
 			}
 			else if (this->has_value() && !other.has_value())
 			{
-				this->destruct();
+        this->reset();
 			}
       return *this;
     }
     ~optional() MY_NOEXCEPT
     {
-      if (this->is_constructed()) this->destruct();
+      this->reset();
     }
   };
 
   template <class T, template <class> class StoragePolicy>
   void swap(optional<T, StoragePolicy>& o1, optional<T, StoragePolicy>& o2) MY_NOEXCEPT { o1.swap(o2); }
+
+  template <class T1, class T2, template <class> class Storage1, template <class> class Storage2>
+  bool operator==(const optional<T1, Storage1>& x, const optional<T2, Storage2>& y)
+  {
+    if (x->has_value() != y->has_value()) return false;
+    else if (x->has_value()) return true;
+    else return *x == *y;
+  }
+  template <class T1, class T2, template <class> class Storage1, template <class> class Storage2>
+  bool operator!=(const optional<T1, Storage1>& x, const optional<T2, Storage2>& y)
+  {
+    return !(x == y);
+  }
+  template <class T1, class T2, template <class> class Storage1, template <class> class Storage2>
+  bool operator<(const optional<T1, Storage1>& x, const optional<T2, Storage2>& y)
+  {
+    if (!y) return false;
+    else if (!x) return true;
+    else return *x < *y;
+  }
+  template <class T1, class T2, template <class> class Storage1, template <class> class Storage2>
+  bool operator>(const optional<T1, Storage1>& x, const optional<T2, Storage2>& y)
+  {
+    if (!x) return false;
+    else if (!y) return true;
+    else return *x > *y;
+  }
+  template <class T1, class T2, template <class> class Storage1, template <class> class Storage2>
+  bool operator<=(const optional<T1, Storage1>& x, const optional<T2, Storage2>& y)
+  {
+    if (!x) return true;
+    else if (!y) return false;
+    else return *x <= *y;
+  }
+  template <class T1, class T2, template <class> class Storage1, template <class> class Storage2>
+  bool operator>=(const optional<T1, Storage1>& x, const optional<T2, Storage2>& y)
+  {
+    if (!y) return true;
+    else if (!x) return false;
+    else return *x >= *y;
+  }
+
+  template <class T, template <class> class Storage>
+  bool operator==(const optional<T, Storage>& x, nullopt_t) MY_NOEXCEPT { return !x; }
+  template <class T, template <class> class Storage>
+  bool operator==(nullopt_t, const optional<T, Storage>& x) MY_NOEXCEPT { return !x; }
+  template <class T, template <class> class Storage>
+  bool operator!=(const optional<T, Storage>& x, nullopt_t) MY_NOEXCEPT { return static_cast<bool>(x); }
+  template <class T, template <class> class Storage>
+  bool operator!=(nullopt_t, const optional<T, Storage>& x) MY_NOEXCEPT { return static_cast<bool>(x); }
+  template <class T, template <class> class Storage>
+  bool operator<(const optional<T, Storage>& x, nullopt_t) MY_NOEXCEPT { return false; }
+  template <class T, template <class> class Storage>
+  bool operator<(nullopt_t, const optional<T, Storage>& x) MY_NOEXCEPT { return static_cast<bool>(x); }
+  template <class T, template <class> class Storage>
+  bool operator<=(const optional<T, Storage>& x, nullopt_t) MY_NOEXCEPT { return !static_cast<bool>(x); }
+  template <class T, template <class> class Storage>
+  bool operator<=(nullopt_t, const optional<T, Storage>& x) MY_NOEXCEPT { return true; }
+  template <class T, template <class> class Storage>
+  bool operator>(const optional<T, Storage>& x, nullopt_t) MY_NOEXCEPT { return !static_cast<bool>(x); }
+  template <class T, template <class> class Storage>
+  bool operator>(nullopt_t, const optional<T, Storage>& x) MY_NOEXCEPT { return false; }
+  template <class T, template <class> class Storage>
+  bool operator>=(const optional<T, Storage>& x, nullopt_t) MY_NOEXCEPT { return true; }
+  template <class T, template <class> class Storage>
+  bool operator>=(nullopt_t, const optional<T, Storage>& x) MY_NOEXCEPT { return !static_cast<bool>(x); }
+
+  template <class T, class U, template <class> class Storage>
+  bool operator==(const optional<T, Storage>& x, const U& v) { return x->has_value() ? *x == v : false; }
+  template <class T, class U, template <class> class Storage>
+  bool operator==(const U& v, const optional<T, Storage>& x) { return x->has_value() ? v == *x : false; }
+  template <class T, class U, template <class> class Storage>
+  bool operator!=(const optional<T, Storage>& x, const U& v) { return !(x == v); }
+  template <class T, class U, template <class> class Storage>
+  bool operator!=(const U& v, const optional<T, Storage>& x) { return !(v == x); }
+  template <class T, class U, template <class> class Storage>
+  bool operator<(const optional<T, Storage>& x, const U& v) { return x->has_value() ? *x < v : true; }
+  template <class T, class U, template <class> class Storage>
+  bool operator<(const U& v, const optional<T, Storage>& x) { return x->has_value() ? v < *x : false; }
+  template <class T, class U, template <class> class Storage>
+  bool operator<=(const optional<T, Storage>& x, const U& v) { return x->has_value() ? *x <= v : true; }
+  template <class T, class U, template <class> class Storage>
+  bool operator<=(const U& v, const optional<T, Storage>& x) { return x->has_value() ? v <= *x : false; }
+  template <class T, class U, template <class> class Storage>
+  bool operator>(const optional<T, Storage>& x, const U& v) { return x->has_value() ? *x > v : false; }
+  template <class T, class U, template <class> class Storage>
+  bool operator>(const U& v, const optional<T, Storage>& x) { return x->has_value() ? v > *x : true; }
+  template <class T, class U, template <class> class Storage>
+  bool operator>=(const optional<T, Storage>& x, const U& v) { return x->has_value() ? *x >= v : false; }
+  template <class T, class U, template <class> class Storage>
+  bool operator>=(const U& v, const optional<T, Storage>& x) { return x->has_value() ? v >= *x : true; }
 }
 
 #endif
