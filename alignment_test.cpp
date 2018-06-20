ec929ac17b5073f78e4dadf68b94df6844a30411
ho-ri1991 <hage.1991@hotmail.co.jp>
2018-06-20 22:36:34 +0900

    add test of alignment_of

--- alignment_test.cpp
+++ alignment_test.cpp
@@ -1,22 +1,20 @@
-#include "aligned_storage.hpp"
-#include <string>
-#include <vector>
-#include <iostream>
-#include <sstream>
+#include "alignment_of.hpp"
+#include "static_assert.hpp"
 
-struct Test1 {
-  std::vector<int> v;
-  std::size_t s;
-  long double ld;
-};
+using namespace my::type_traits;
 
 int main() {
-  my::type_traits::aligned_storage<Test1> t;
-  // Test1
-  std::cout << alignof(decltype(t)) << std::endl;
-  std::cout << alignof(Test1) << std::endl;
-  std::cout << sizeof(decltype(t)) << std::endl;
-  std::cout << sizeof(Test1) << std::endl;
+#if defined(__GNUC__)
+  STATIC_ASSERT(__alignof__ (char) == alignment_of<char>::value, char_alignment_failuer);
+  STATIC_ASSERT(__alignof__ (short) == alignment_of<short>::value, short_alignment_failuer);
+  STATIC_ASSERT(__alignof__ (int) == alignment_of<int>::value, int_alignment_failuer);
+  STATIC_ASSERT(__alignof__ (long) == alignment_of<long>::value, long_alignment_failuer);
+  STATIC_ASSERT(__alignof__ (long long) == alignment_of<long long>::value, long_long_alignment_failuer);
+  STATIC_ASSERT(__alignof__ (float) == alignment_of<float>::value, float_alignment_failuer);
+  STATIC_ASSERT(__alignof__ (double) == alignment_of<double>::value, double_alignment_failuer);
+  STATIC_ASSERT(__alignof__ (long double) == alignment_of<long double>::value, long_double_alignment_failuer);
+  STATIC_ASSERT(__alignof__ (void*) == alignment_of<void*>::value, pointer_alignment_failuer);
+#endif
   return 0;
 }
 
